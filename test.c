f462f61363be67ac7f429f1a40e72f1e90856078
Hans Bourgeois <hbourgeo@fu-r5-p2.s19.be>
2022-01-21 18:01:20 +0100

    update ft_lstadd_front

--- test.c
+++ test.c
@@ -7,36 +7,54 @@
 
 typedef struct s_list 
 {
-	void *content;
-	struct s_list *next;
+    void *content;
+    struct s_list *next;
 } t_list;
 
-t_list *ft_lstnew(void *content)
+t_list    *ft_lstnew(void *content)
 {
-	t_list *new;
+    t_list    *ptr;
 
-	new = malloc(sizeof(t_list));
-	if (!new)
-		return (NULL);
-	new->content = content;
-	new->next = NULL;
-
-	return (new);
+    ptr = malloc(sizeof(t_list));
+    if (!ptr)
+        return (NULL);
+    ptr->content = content;
+    ptr->next = NULL;
+    return (ptr);
 }
 
-int main(void)
+void ft_lstadd_front(t_list **lst, t_list *new)
 {
-	int c = -2147483648;
-	// size_t n;
-	// n = 0;
-	ft_putnbr_fd(c, 1);
-	// printf("c = %s\n", c);
-	// n++;
-	// printf("=====================================================\n");
+    new->next = *lst;
+    *lst = new;
+}
 
-	// char str3[] = "beabtt";
-	// char str4[] = "veaatt";
-	// c = strncmp(str3, str4, 0);
-	// printf("c = %d\n", c );
-	printf("=====================================================\n");
+int main() {
+  
+  t_list *l1;
+  t_list *add1;
+  t_list *add2;
+  int num0;
+  int num1;
+  int num2;
+  
+  num0 = 0;
+  num1 = 42;
+  num2 = 84;
+  
+  
+  l1 = NULL;
+  add1 = ft_lstnew(&num1);
+  add2 = ft_lstnew(&num2);
+  
+  ft_lstadd_front(&l1, add1);
+  ft_lstadd_front(&add1, add2);
+  while (add1 != NULL)
+  {
+        printf("%d -> ", *(int *)(add1->content));
+        add1 = add1->next;
+  }
+  
+
+  return 0;
 }
